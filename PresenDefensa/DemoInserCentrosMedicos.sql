a62ce11b4d7422edc4cad2df707fbd614ab96dad
ivannia-alvarado <mana1893@hotmail.com>
2014-11-23 18:55:03 -0600

    ...

--- PresenDefensa/DemoInserCentrosMedicos.sql
+++ PresenDefensa/DemoInserCentrosMedicos.sql
@@ -3,14 +3,17 @@ insert
 into centros_medicos (nombre, geom)
 values ('polígono', 'POLYGON((-20 -20, -20 20, 20 20, 20 -20, -20 -20))');
 
-select * from centros_medicos where nombre = 'centro médico con polígono';
+select * from centros_medicos where nombre = 'polígono';
 
--- Si inserto un polígono no hace la inserción, pues solo se permiten puntos.
+-- Si inserto un punto no contenido dentro de un área de salud, 
+-- no se puede porque viola la participación total.
+select geom.STX, geom.STY from centros_medicos;
+
 insert 
 into centros_medicos (nombre, geom)
-values ('punto fuera del país', geometry::STPointFromText('POINT(0 10)',0));
+values ('punto mal ubicado', geometry::STPointFromText('POINT(0 10)',0));
 
-select * from centros_medicos where nombre = 'punto fuera del país';
+select * from centros_medicos where nombre = 'punto mal ubicado';
 
 -- Si inserto un hospital del shape (válido) 
 insert 
@@ -18,4 +21,7 @@ into centros_medicos (nombre, geom)
 values ('centro medico valido', (select top 1 geom from clinicas2008crtm05) );
 
 select * from centros_medicos where nombre = 'centro medico valido';
+delete from centros_medicos where nombre = 'centro medico valido';
+
+
 
